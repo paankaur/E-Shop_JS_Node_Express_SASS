a38d55ffde40a573c558307dc2736690f7e9f327
Paan Kaur <paan.kaur@gmail.com>
2024-11-20 21:09:23 +0200

    add decimal points

--- ylesanne1/main.js
+++ ylesanne1/main.js
@@ -8,9 +8,21 @@ class Product {
   }
 
   describe() {
-    return `Title: ${this.title}, Price: €${this.price}, Category: ${this.category}`;
+    return `Title: ${this.title}, Price: €${this.price.toFixed(2)}, Category: ${this.category}`;
   }
   static discountedPrice(price, discount) {
-    return price - (price * discount / 100);
+    let discounted = price - (price * discount / 100);
+    return `€${discounted.toFixed(2)}`;
   }
 }
+const laptop = new Product("Läppar", 100, "Elektroonika");
+
+
+
+console.log(Product.discountedPrice(laptop.price, 10));
+console.log(laptop.describe(), Product.discountedPrice(laptop.price, 10));
+
+class Cart {
+  
+}
+// class extends?
