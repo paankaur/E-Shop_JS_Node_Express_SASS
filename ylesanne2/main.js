85449817a9a788693762e414bec04ae4275c17f6
Paan Kaur Riives <paan.kaur@gmail.com>
2024-11-26 12:04:43 +0200

    DOM pooleli

--- ylesanne2/main.js
+++ ylesanne2/main.js
@@ -4,14 +4,30 @@ import { Cart } from "./cart.js";
 import { Customer } from "./customer.js";
 
 //tegin tooted
-const laptop = new Product(1, "rüperaal", 699.75, "Elektroonika");
-const kuubik = new Product(2, "3x3", 32.90, "Pusle");
 
+const products = [
+    new Product(1, "Rüperaal", 699.75, "Elektroonika"),
+    new Product(2, "3x3", 32.90, "Pusle"),
+    new Product(3, "Tahvelarvuti", 299.00, "Elektroonika"),
+
+];
+//Toodete kuvamine
+ function displayProducts() {
+    const mainDiv = document.getElementById("mainDiv");
+ }
+products.forEach((product) => {
+    const productCard = document.createElement("div")
+    const productTitle.textContent = document.createElement("h3")
+    productCard.innerHTML = `<h3>${product.title}</h3>`
+    mainDiv.append(productTitle);
+    // mainDiv.append(productCard);
+});
+displayProducts();
 //Ostukorv ja lisan tooteid sinna
 const cart = new Cart();
-cart.addProduct(laptop, 1);
-cart.addProduct(kuubik, 5);
-cart.addProduct(kuubik, -3);
+// cart.addProduct(laptop, 1);
+// cart.addProduct(kuubik, 5);
+// cart.addProduct(kuubik, -3);
 
 //Ostukorvi sisuhaldus
 console.log("Kogusumma: ", cart.calculateTotal());
@@ -25,5 +41,21 @@ customer.placeOrder(cart);
 //Tellimuse ajaloo kuvamine
 customer.printOrderHistory();
 
-console.log(kuubik.describe());
-console.log(laptop.describe());
+// console.log(kuubik.describe());
+// console.log(laptop.describe());
+
+const pealkiri = document.getElementById("pealkiri");
+
+document.title = "Kaunis E-Pood!"
+
+pealkiri.style.backgroundColor = "chocolate";
+pealkiri.style.color = "purple";
+pealkiri.style.textAlign = "center";
+
+
+console.log("mu title on: " + "pealkiri");
+
+pealkiri.textContent = "Kaubik"
+
+const mainDiv = document.getElementById("mainDiv");
+console.log("mainDiv");
