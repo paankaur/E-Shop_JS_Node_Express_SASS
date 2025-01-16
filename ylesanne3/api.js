1c9710e077a4f9e1a0e0e62a24b940dacf8a7acc
Paan Kaur Riives <paan.kaur@gmail.com>
2025-01-16 15:37:49 +0200

    oma andmete kasutamine pooleli

--- ylesanne3/api.js
+++ ylesanne3/api.js
@@ -1,61 +1,61 @@
 import { Product } from "./constructors/product.js";
-const BASE_URL = "https://fakestoreapi.com"
+const BASE_URL = "https://fakestoreapi.com";
+// const BASE_URL = 'localhost:3000';
 export const getProductsDataFromJson = async () => {
-    try {
-        const data = await fetch("./data.json");
-        return data.json();
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const data = await fetch("./data.json");
+    return data.json();
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getProductsByCategory = async (category) => {
-    try {
-        const data = await fetch(`${BASE_URL}/products/category/${category}`);
-        const productsData = await data.json();
+  try {
+    const data = await fetch(`${BASE_URL}/products/category/${category}`);
+    // const data = await application.get(`${BASE_URL}/data/products.json/${category}`);
+    const productsData = await data.json();
 
-        const dataObject = productsData.map(
-            (item) => 
-                new Product(
-                    item.id,
-                    item.title,
-                    item.price,
-                    item.category,
-                    item.description,
-                    item.image
-                )
-        );
-        return dataObject;
-
-    } catch (error) {
-        console.error(error);
-    }
+    const dataObject = productsData.map(
+      (item) =>
+        new Product(
+          item.id,
+          item.title,
+          item.price,
+          item.category,
+          item.description,
+          item.image
+        )
+    );
+    return dataObject;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getAllCategory = async () => {
-    try {
-        const data = await fetch(`${BASE_URL}/products/categories`);
-        return data.json();
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const data = await fetch(`${BASE_URL}/products/categories`);
+    return data.json();
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getProductById = async (productId) => {
-    try {
-        const data = await fetch(`${BASE_URL}/products/${productId}`);
-        const productData = await data.json();
-        const dataObject = new Product(
-            productData.id,
-            productData.title,
-            productData.price,
-            productData.category,
-            productData.description,
-            productData.image
-
-        );
-        return dataObject;
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const data = await fetch(`${BASE_URL}/products/${productId}`);
+    const productData = await data.json();
+    const dataObject = new Product(
+      productData.id,
+      productData.title,
+      productData.price,
+      productData.category,
+      productData.description,
+      productData.image
+    );
+    return dataObject;
+  } catch (error) {
+    console.error(error);
+  }
 };
